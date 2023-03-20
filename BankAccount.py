401d3b6e7bd75da193a42b9e42f0eaa51cb6570c
Kolbae1 <115962266+Kolbae1@users.noreply.github.com>
2023-03-20 09:44:06 -0400

    Add files via upload

---
+++ BankAccount.py
@@ -0,0 +1,7 @@
+class BankAccount: 
+
+    OVERDRAFT_FEE = 20.00
+    INTEREST_RATE = 7.5
+
+    def __init__(self, firstName, lastName, accountNum, transactions, overdrawCount, balance):
+        pass
