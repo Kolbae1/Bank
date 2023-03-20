392040ebacd1034f5b6590fe9215dd1b8abcc2de
Caleb <122816375+CalebColeman22@users.noreply.github.com>
2023-03-20 17:51:01 -0400

    Update BankAccount.py
    Fixed bug where withdrawal would be called twice, think I finished all of d as well.

--- BankAccount.py
+++ BankAccount.py
@@ -78,20 +78,23 @@ class BankAccount:
         self._transactions.append(trans)
 
 
-    def widthdrawal(self): 
+    def withdrawal(self): 
         trans = Transaction(2)
 
         if(trans.getAmount() > self._balance + 250):
             print("Transaction is denied")
         
-        if(self._balance > 0):
-            trans = Transaction(2)
+        elif(self._balance > 0):
+           # trans = Transaction(2)
             self._transactions.append(trans)
             self._balance = self._balance - trans.getAmount()
             
             if(self._balance < 0):
                 self._overdrawNum = self._overdrawNum + 1
                 penaltyTrans = Transaction(2, BankAccount.OVERDRAFT_FEE)
+                self._balance = self._balance - BankAccount.OVERDRAFT_FEE
+                print("Account is overdrawn.")
+                self._transactions.append(trans)
 
 
 
