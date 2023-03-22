2e7e703823d2c2072061239a4afc7cd82901c662
Kolbae1 <115962266+Kolbae1@users.noreply.github.com>
2023-03-22 14:12:40 -0400

    Update test.py

--- test.py
+++ test.py
@@ -6,10 +6,19 @@ from BankAccount import *
 #print(trans1.getDate())
 
 bank1 = BankAccount()
-print(bank1.__str__())
+bank2 = BankAccount()
 
 bank1.deposit()
-bank1.deposit()
+bank2.deposit()
 
 print(bank1.__str__())
-#print(bank2.getAccountNum())
+print(bank2.__str__())
+
+
+bank1.transfer(bank2)
+
+#bank1.interest()
+
+print(bank1.__str__())
+print("")
+print(bank1.getBalance())
